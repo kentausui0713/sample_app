dd88c569507f6de1c9bbbc7640a09e709df6a3e8
Ubuntu <ubuntu@ip-172-31-41-80.ap-northeast-1.compute.internal>
2022-04-04 06:12:22 +0000

    Add full title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #タイトルが空でも|が最初に入らないためのカスタムヘルパー
+  def full_title(page_title='')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
