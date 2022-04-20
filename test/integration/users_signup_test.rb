d5293b206d091e1710f1b60cb9418fb8f6168251
kentausui <usuikenta100@gmail.com>
2022-04-20 05:34:51 +0000

    finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: { name: "",
+                                        email: "user@invalid",
+                                        password: "foo",
+                                        password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    
+    assert_difference 'User.count', 1 do
+      post users_path, params: {user: { name: "Rails Tutorial",
+                                        email: "example@railstutorial.org",
+                                        password: "exampleuser",
+                                        password_confirmation: "exampleuser"} }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.blank?
+    # assert_select 'div.alert-success'と同じ（flashが存在するか）　これは成功時のクラス名指定
+    
+  end
+end
