d00d2a6b708e963285fd95912df8d1eb86cd2adb
kentausui <usuikenta100@gmail.com>
2022-07-02 07:54:50 +0000

    [fix]smtp_setting => smtp_settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   host = "https://serene-chamber-39177.herokuapp.com/"
   config.action_mailer.default_url_options = { host: host}
-  ActionMailer::Base.smtp_setting = {
+  ActionMailer::Base.smtp_settings = {
     :port           =>  ENV['MAILGUN_SMTP_PORT'],
     :address        =>  ENV['MAILGUN_SMTP_SERVER'],
     :user_name      =>  ENV['MAILGUN_SMTP_LOGIN'],
